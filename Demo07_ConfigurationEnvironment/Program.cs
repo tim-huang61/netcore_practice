47411f0219484235799c391e1bb51afae8f1d1ee
tim <tim.huang61@gmail.com>
2020-04-22 10:36:13 +0800

    configuration environment

---
+++ Demo07_ConfigurationEnvironment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo07_ConfigurationEnvironment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddEnvironmentVariables();
+
+            var configurationRoot = builder.Build();
+            Console.WriteLine(configurationRoot["key1"]);
+            Console.WriteLine(configurationRoot["key2"]);
+            Console.WriteLine(configurationRoot["section1:key3"]);
+            Console.WriteLine(configurationRoot["tim_key1"]);
+
+            // 環境變數加上前綴
+            builder.AddEnvironmentVariables("tim_");
+            var root = builder.Build();
+            Console.WriteLine(root["key1"]);
+
+            Console.Read();
+        }
+    }
+}
