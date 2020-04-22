744f9c9b95d1313e751baaee22a9b2a580eb33ec
tim <tim.huang61@gmail.com>
2020-04-22 11:23:00 +0800

    demo json configuration down.

---
+++ Demo08_JsonConfiguration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Demo08_JsonConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            // configurationBuilder.Add(new JsonConfigurationSource()
+            // {
+            //     Path = "appsettings.json"
+            // });
+            configurationBuilder.AddJsonFile("appsettings.json");
+            var configurationRoot = configurationBuilder.Build();
+            Console.WriteLine(configurationRoot["key1"]);
+            Console.WriteLine(configurationRoot["key2"]);
+            Console.WriteLine(configurationRoot["section1:key3"]);
+            Console.Read();
+        }
+    }
+}
